301bfaf0da068a4c8995821948294d66be6d1928
loramisautova <misautova.lora@gmail.com>
2021-10-08 22:33:25 +0300

    add const.

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -1,5 +1,5 @@
 /** Application name. */
-export const APP_NAME = 'Crispy Films'
+export const APP_NAME = 'Crispy Films';
 
 /** Base API prefix. */
 export const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3';
